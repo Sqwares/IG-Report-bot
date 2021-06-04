045514482fe62cc2a2d9c0a0fdf277c1ae38d845
Sqwares <83078854+Sqwares@users.noreply.github.com>
2021-06-04 22:27:27 +0300

    Add files via upload

---
+++ libs/logo.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+#!/usr/bin/env python3
+from colorama import Fore, Back, Style
+from random import choice
+
+logo = """
+ █     █░ ▄▄▄       ██▀███  ▓█████   ██████ 
+▓█░ █ ░█░▒████▄    ▓██ ▒ ██▒▓█   ▀ ▒██    ▒ 
+▒█░ █ ░█ ▒██  ▀█▄  ▓██ ░▄█ ▒▒███   ░ ▓██▄   
+░█░ █ ░█ ░██▄▄▄▄██ ▒██▀▀█▄  ▒▓█  ▄   ▒   ██▒
+░░██▒██▓  ▓█   ▓██▒░██▓ ▒██▒░▒████▒▒██████▒▒
+░ ▓░▒ ▒   ▒▒   ▓▒█░░ ▒▓ ░▒▓░░░ ▒░ ░▒ ▒▓▒ ▒ ░
+  ▒ ░ ░    ▒   ▒▒ ░  ░▒ ░ ▒░ ░ ░  ░░ ░▒  ░ ░
+  ░   ░    ░   ▒     ░░   ░    ░   ░  ░  ░  
+    ░          ░  ░   ░        ░  ░      ░  
+                                             """
+
+
+
+def print_logo():
+    print(Fore.RED + Style.BRIGHT + logo + Style.RESET_ALL + Style.BRIGHT +"\n")
+    print(Fore.YELLOW + "      By Sqwares my Discord Server Link:https://discord.gg/nwKbvBgJSZ "+ Style.RESET_ALL + Style.BRIGHT)
+    print(Style.RESET_ALL + Style.BRIGHT, Style.BRIGHT)
